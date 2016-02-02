8339fa41dcc8d6084e50b058ad1544c57c32263b
Chloe Horgan <super.chloe@gmail.com>
2016-02-02 11:02:58 -0500

    Created map view controller

---
+++ Rotten Mangoes/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Rotten Mangoes
+//
+//  Created by Chloe on 2016-02-02.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
